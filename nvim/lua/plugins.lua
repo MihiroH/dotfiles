a63a753aef407c384fa44c2315f7305dd3dc3f33
MihiroH <mihiro.hashimoto.1125@gmail.com>
2026-02-24 00:20:09 +0900

    chore(nvim): remove render-markdown plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -126,12 +126,6 @@ return {
   },
 
   {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
-    config = function() require('config.render-markdown') end,
-  },
-
-  {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     build = 'cd app && bun install',
