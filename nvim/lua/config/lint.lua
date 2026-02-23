642d384f07c49883003c8f372c1980b1d222b9be
MihiroH <mihiro.hashimoto.1125@gmail.com>
2026-02-23 16:48:23 +0900

    chore(nvim): remove linter-by-ft config from nvim-lint

--- nvim/lua/config/lint.lua
+++ nvim/lua/config/lint.lua
@@ -1,12 +1,5 @@
 local lint = require('lint')
 
-lint.linters_by_ft = {
-  css = { 'stylelint' },
-  scss = { 'stylelint' },
-  python = { 'flake8' },
-  go = { 'staticcheck' },
-}
-
 vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
   callback = function()
     lint.try_lint()
